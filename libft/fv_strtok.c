58f0e83e331b16ac24fb4aaab3f29a677f4675b2
Flavia Voicu <fvoicu@1-E-31.42heilbronn.de>
2023-10-03 11:08:03 +0200

    strtok working// needed refactoring

--- libft/fv_strtok.c
+++ libft/fv_strtok.c
@@ -6,7 +6,7 @@
 /*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 18:28:47 by fvoicu            #+#    #+#             */
-/*   Updated: 2023/10/02 19:09:02 by fvoicu           ###   ########.fr       */
+/*   Updated: 2023/10/03 11:06:41 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -35,40 +36,85 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-char *fv_strtok(char *str, char *delim)
-{
+// char *fv_strtok(char *str, char *delim)
+// {
+//     static char *buffer = NULL;
+//     char *current_token = NULL;
+// 	bool is_delim = false;
+// 	int i = 0;
+
+// 	if (!str || !delim)
+// 		return (NULL);
+//     if (str)
+//         buffer = str;
+//     if (*buffer == NULL || *buffer == '\0')
+// 		return (NULL);
+// 	while (*buffer)
+//     {
+// 		is_delim = false;
+// 		while(delim[i] != '\0')
+// 		{
+// 			if (*buffer == delim[i]) 
+// 			{
+//                 is_delim = true;
+//                 *buffer = '\0';
+//                 break;
+// 			}
+// 			i++;
+// 		}
+//     }
+// 	if (!is_delim && !current_token)
+// 		current_token = buffer;
+// 	buffer++;
+
+// 	if (is_delim)
+// 	{
+// 		if(*buffer != '\0')
+// 			buffer++;
+// 			return (current_token);
+// 	}
+//     // if (*current_token == '\0')
+//     // {
+//     //     buffer = NULL;
+//     //     return (NULL);
+//     // }
+//     // else
+//     // {
+//     //     buffer = NULL;
+//     //     return (current_token);
+//     // }
+// }
+
+char *fv_strtok(char *str, const char *delim) {
     static char *buffer;
-    char *current_token;
-	
-	if (!str || !delim)
-		return (NULL);
+    char *token_start, *ptr;
+    
     if (str)
         buffer = str;
-    current_token = buffer;
-    while (*buffer)
-    {
-        if (ft_strchr(delim, *buffer))
-        {
-            *buffer = '\0';
-            if (*current_token == '\0')
-                current_token = buffer;
-            else
-                return (current_token);
-        }
+    if (!buffer || !delim)
+        return NULL;
 
-        buffer++;
+    for ( ; *buffer != '\0'; buffer++) {
+        ptr = strchr(delim, *buffer);
+        if (ptr == NULL)
+            break;
     }
-    if (*current_token == '\0')
-    {
-        buffer = NULL;
-        return (NULL);
+    if (*buffer == '\0') 
+        return NULL;
+
+    token_start = buffer;
+
+    for ( ; *buffer != '\0'; buffer++) {
+        ptr = strchr(delim, *buffer);
+        if (ptr != NULL) {
+            *buffer = '\0';
+            buffer++;
+            break;
+        }
     }
-    else
-    {
-        buffer = NULL;
-        return (current_token);
-    }
+    return token_start;
 }
+
 int main()
 {
 	char str[] = "Hello World, how are you?";
@@ -80,68 +126,71 @@ int main()
 		printf("%s\n", token);
 		token = fv_strtok(NULL, delim);
 	}
+
+	printf("\n------------\n");
+
 	char str1[] = "Hello World, how are you?";
 	char delim1[] = " ";
 	char *token1;
-	
-	printf("\n------------\n");
-	
+
 	token1 = strtok(str1, delim1);
 	while(token1 != NULL) {
 		printf("%s\n", token1);
 		token1 = strtok(NULL, delim1);
 	}
 	
-
-//   char str2[] = "Hello,,World";
-//   char delim2[] = ",";
+	printf("\n------------\n");
+	char str2[] = "Hello,,World";
+	char delim2[] = ",";
   
-//   token = fv_strtok(str2, delim2);
-//   while(token != NULL) {
-//     printf("%s\n", token);
-//     token = fv_strtok(NULL, delim2);
-//   }
+	token = fv_strtok(str2, delim2);
+	while(token != NULL) {
+    	printf("%s\n", token);
+    	token = fv_strtok(NULL, delim2);
+	}
 
-//   char str3[] = "Hello,,,World";
-//   char delim3[] = ",";
+	printf("\n------------\n");
+	char str3[] = "Hello,,,World";
+	char delim3[] = ",";
   
-//   token = fv_strtok(str3, delim3);
-//   while(token != NULL) {
-//     printf("%s\n", token);
-//     token = fv_strtok(NULL, delim3);
-//   }
+	token = fv_strtok(str3, delim3);
+	while(token != NULL) {
+		printf("%s\n", token);
+		token = fv_strtok(NULL, delim3);
+	}
+	printf("\n------------\n");
+	char str4[] = ",";
+	char delim4[] = ",";
 
-//   char str4[] = ",";
-//   char delim4[] = ",";
-
-//   token = fv_strtok(str4, delim4);
-//   if(token == NULL) {
-//     printf("No tokens\n");
-//   }
-
-//   char *str5 = NULL;
-//   char delim5[] = " ";
+	token = fv_strtok(str4, delim4);
+	if(token == NULL) {
+		printf("No tokens\n");
+	}
+	
+	printf("\n------------\n");
+	char *str5 = NULL;
+	char delim5[] = " ";
   
-//   token = fv_strtok(str5, delim5);
-//   if(token == NULL) {
-//     printf("Null string returns null\n");
-//   }
+	token = fv_strtok(str5, delim5);
+	if(token == NULL) {
+		printf("Null string returns null\n");
+	}
+	printf("\n------------\n");
+	char str6[] = "Hello World";
+	char *delim6 = NULL;
 
-//   char str6[] = "Hello World";
-//   char *delim6 = NULL;
+	token = fv_strtok(str6, delim6);
+	if(token == NULL) {
+		printf("Null delimiter returns null\n");
+	}
+	printf("\n------------\n");
+	char str7[] = "Hello World";
+	char delim7[] = "";
 
-//   token = fv_strtok(str6, delim6);
-//   if(token == NULL) {
-//     printf("Null delimiter returns null\n");
-//   }
+	token = fv_strtok(str7, delim7);
+	if(token != NULL) {
+		printf("%s\n", token);
+	}
 
-//   char str7[] = "Hello World";
-//   char delim7[] = "";
-
-//   token = fv_strtok(str7, delim7);
-//   if(token != NULL) {
-//     printf("%s\n", token);
-//   }
-
-  return 0;
+	return 0;
 }
