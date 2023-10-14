3e9a275be0c1abb09a646e54177854429058ac3d
Flavia Voicu <fvoicu@3-C-9.42heilbronn.de>
2023-10-14 05:44:23 +0200

    some changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:46:32 by fvoicu            #+#    #+#             */
-/*   Updated: 2023/10/12 15:47:44 by fvoicu           ###   ########.fr       */
+/*   Updated: 2023/10/14 05:43:41 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,189 +16,79 @@
 
 //parsy_parse, get_cmd
 
-static char	***prep_cmd(t_info *pipex, int arg_idx)
+// static char	***prep_cmd(t_info *pipex, int arg_idx)
+// {
+// 	char	**cmd;
+
+// 	if (ft_strncmp(pipex->argv[arg_idx], "./", 2))
+// 	{
+// 		cmd = ft_split(parsy_parse(pipex->argv[arg_idx]), ' ');
+// 		cmd = get_cmd(cmd);
+// 	}
+// 	else
+// 		cmd =
+// }
+
+static void	exec_cmd(int *fd, char *cmd_path, char **args, char **env)
 {
 	char	**cmd;
-
-	if (ft_strncmp(pipex->argv[arg_idx], "./", 2))
-	{
-		cmd = ft_split(parsy_parse(pipex->argv[arg_idx]), ' ');
-		cmd = get_cmd(cmd);
-	}
-	else
-		cmd = 
-}
-
-static void	exec_cmd(t_info *pipex, char **args, char **env)
-{
-	dup2(pipex->fd[0], STDIN_FILENO);
-	dup2(pipex->fd[1], STDOUT_FILENO);
-	if (pipex->fd[0] != STDIN_FILENO)
-		close(pipex->fd[0]);
-	if (pipex->fd[1] != STDOUT_FILENO)
-		close(pipex->fd[1]);
-	execve (pipex->cmd_path, args, env);
-	perror ("execve"); 
-	exit (1);
-}
-
-void	pipex(t_info *pipex, char **env, char *input, char *output)
-{
-	int prev_fd;
-	int i;	
+	char 	*path;
 	
-	i = 2;
-	pipex->pid = malloc (sizeof(int) * (pipex->argc - 3));
-	while (i < (pipex->argc - 1))
-	{
-		pipex->cmd_path = get_path(pipex->argv[i], env);
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	
+	if (fd[0] != STDIN_FILENO)
+		close(fd[0]);
+	if (fd[1] != STDOUT_FILENO)
+		close(fd[1]);
+	// cmd = prep_cmd(cmd_path, args, env);, ' ');
+	if (access(cmd, X_OK | F_OK) < 0)
+		path = get_path(cmd_path, env);
 		
-		pipe(pipex->fd);
-		
-		if (!(pipex->pid = fork()))
-		{
-			if(prev_fd != -1)
-				close(prev_fd);			
-		}
-		
-		
-		++i;
-	}
+	if (execve (cmd_path, args, env) == -1)
+		error("Failed to execute command.", NULL, 1);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void	pipex(t_info *pipex, char	*input, char	**argv, char **env, char *output)
+void	pipex(int *input, char **env, char **argv)
 {
-	int	fd_in;
-	int	fd_out;
-	int	prev_pipe_read_end;
-	int	i;
-	char	*cmd_path;
+	pid_t	pid;
+	int		fd[2];
+	int		i;
 
-	i = 2; //index for argv
-	prev_pipe_read_end = -1; //For first command ---> input file
-	while (i < pipex->argc - 1)
+	while (argv [++i + 1])
 	{
-		cmd_path = get_path(argv[i], env);
-		pipe(pipex->fds);
-		
-		if (!(pipex->pid = fork())) //-->child
+		if (pipe(fd))
+			error("Failed to create pipe.", NULL, 1);
+		pid = fork();
+		if (pid == -1)
+			error("Failed to create subprocess.", NULL, 1);
+		if (pid == 0)
 		{
-			if (prev_pipe_read_end != -1) //not first command
-			{
-				dup2(prev_pipe_read_end, STDIN_FILENO);
-				close(prev_pipe_read_end);
-			}
-			else //for 1st command read from input file
-			{
-				fd_in = open(input, O_RDONLY);
-				if (fd_in < 0)
-				{
-					perror("open input");
-					exit(1);
-				}
-				dup2(fd_in, STDIN_FILENO);
-				close(fd_in);
-			}
-			if (i != pipex->argc - 2) // Not last command
-				dup2(pipex->fds[1], STDOUT_FILENO);
-			else // For last commnd, write to output file
-			{
-				fd_out = open(output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-				if (fd_out < 0)
-				{
-					perror("open output");
-					exit(1);
-				}
-				dup2(fd_out, STDOUT_FILENO);
-				close(fd_out);
-			}
-			close(pipex->fds[0]);
-			close(pipex->fds[1]);
-			execve(cmd_path, argv + i, env);
-			perror("execve");
-			exit(1);
+			exec_cmd(argv, env, fd); //-->check this out	
+			exit (0);
 		}
-		else //-->parent
-		{
-			close(pipex->fds[1]);
-			if (prev_pipe_read_end != -1)
-				close(prev_pipe_read_end);
-			prev_pipe_read_end = pipex->fds[0];	
-		}
-		free(cmd_path);
-		++i; // --->next command
-	}
-	i = 2; //reset counter
-	while (i < pipex->argc - 1)
-	{
-		wait(NULL); //wait for all children to finish
-		++i;
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(pid, NULL, 0);
+		if (input)
+			free(input);
+		input = NULL;
 	}
 }
-
-
 
 int	main(int argc, char **argv, char **env)
 {
-	t_info t_pipex;
 	char *input;
-	char *output;
-	
-	if (argc < 5)
+
+	if (argc < 4 || (is_here_doc(argv[1]) && argc < 5))
 	{
-		write(2, "Invalid number of arguments\n", 28);
-		exit(1);
+		error("Invalid arguments", NULL, 1);
+		return (1);
 	}
-	t_pipex.argc = argc;
-	t_pipex.cmd_path = argv;
-	t_pipex.cmd_arg = argv + 2;
-	t_pipex.here_doc = false;
-	
-	input = argv[1];
-	output = argv[argc - 1];
 
-	pipex(&t_pipex , input, argv, env, output);
-	return (0);
+	input = some_func_i_need;
+	if (is_here_doc(argv[1]))
+		here_doc(argv++);
+	pipex(input, env, arg);
 }
-
-// #include <stdio.h>
-
-// extern char **environ;
-
-// int main(void)
-// {
-//     t_info pipex_info;
-//     char *input = "input.txt";
-//     char *output = "output.txt";
-//     char *argv[] = {"pipex", "cat", "grep test", NULL}; // pipex is just a placeholder for argv[0]
-//     char **env = environ;
-
-
-//     pipex(&pipex_info, input, argv, env, output);
-
-//     // Read the output file to see if it contains the line with "test".
-//     FILE *fp = fopen(output, "r");
-//     char line[256];
-//     while (fgets(line, sizeof(line), fp)) {
-//         printf("%s", line); // Should print "this is a test" if everything worked correctly
-//     }
-//     fclose(fp);
-
-//     return 0;
-// }
--- src/utils.c
+++ src/utils.c
@@ -6,25 +6,29 @@
 /*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:02:22 by fvoicu            #+#    #+#             */
-/*   Updated: 2023/10/12 19:12:13 by fvoicu           ###   ########.fr       */
+/*   Updated: 2023/10/14 05:24:15 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-void	error(char *msg, char **cmd, int exit_code)
+//TO DO: ft_strncmp doesn t handle the case where s1 is shorter than s2 -->falsely evaluating to true
+void	error(char *error, char **cmd, int exit_code)
 {
-	char	*error;
+	char	*message;
 
-	// if (cmd != NULL)
-	// {
-	// 	free_array(cmd);
-	// }
-	error = msg;
-	msg = fv_strjoin_and_free("Error: ", error);
-	ft_putendl_fd(msg, 2);
-	free(msg);
+	if (cmd != NULL)
+	{
+		fv_free_array(cmd);
+	}
+	message = fv_strjoin_and_free("Error: ", error);
+	ft_putendl_fd(message, 2);
+	free(message);
 	exit(exit_code);
+}
+bool	is_here_doc(const char *arg)
+{
+	return (!ft_strncmp(arg, "here_doc", 9));
 }
 
 char	*parsy_parse(const char *input)
@@ -53,80 +57,3 @@ char	*parsy_parse(const char *input)
 	result[j] = '\0';
 	return (result);
 }
-
-// #include <stdio.h>
-// #include <stdbool.h>
-// #include <string.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// bool test_parsy_parse(const char *input, const char *expected) {
-//     char *result = parsy_parse(input);
-//     bool is_equal = strcmp(result, expected) == 0;
-
-//     printf("Input: \"%s\" | Expected: \"%s\" | Result: \"%s\" | %s\n",
-//            input, expected, result, is_equal ? "PASS" : "FAIL");
-//     free(result);
-
-//     return is_equal;
-// }
-
-// int main() {
-//     bool all_passed = true;
-
-//     // Test cases
-//     all_passed &= test_parsy_parse("Hello  world", "Hello world");
-//     all_passed &= test_parsy_parse("    Leading spaces", "Leading spaces");
-//     all_passed &= test_parsy_parse("Trailing spaces    ", "Trailing spaces");
-//     all_passed &= test_parsy_parse("   Multiple   spaces   everywhere   ", "Multiple spaces everywhere");
-//     all_passed &= test_parsy_parse("", "");
-//     all_passed &= test_parsy_parse("     ", "");
-//     all_passed &= test_parsy_parse("A", "A");
-//     all_passed &= test_parsy_parse(" A", "A");
-//     all_passed &= test_parsy_parse("A ", "A");
-//     all_passed &= test_parsy_parse(" A ", "A");
-//     all_passed &= test_parsy_parse("This    is    a    test    ", "This is a test");
-//     all_passed &= test_parsy_parse("     This    is    another    test", "This is another test");
-//     all_passed &= test_parsy_parse("Space     at    the     end and start    ", "Space at the end and start");
-//     all_passed &= test_parsy_parse("   Only    one   word", "Only one word");
-//     all_passed &= test_parsy_parse("Special  chars!    @ # $ % ^ & *    ( ) ", "Special chars! @ # $ % ^ & * ( )");
-//     // Add the below tests if your fv_is_space function also counts tabs and newlines as spaces
-//     all_passed &= test_parsy_parse("Tabs\t\t\talso count\t\tas spaces", "Tabs also count as spaces");
-//     all_passed &= test_parsy_parse("Newlines\n\n\nare\n\nspaces too", "Newlines are spaces too");
-// 	all_passed &= test_parsy_parse("\n\nMultiple\n\nNewlines", "Multiple Newlines");
-//     all_passed &= test_parsy_parse("\t\tTabs\tonly\t\t", "Tabs only");
-//     all_passed &= test_parsy_parse("\n\nMixed\n\twhitespaces\t\t", "Mixed whitespaces");
-//     all_passed &= test_parsy_parse(".    . .   .  . .", ". . . . . .");
-//     all_passed &= test_parsy_parse("! !   !    !  !", "! ! ! ! !");
-//     all_passed &= test_parsy_parse("Hello,   world! How    are  you?  ", "Hello, world! How are you?");
-//     all_passed &= test_parsy_parse("123  456   789", "123 456 789");
-//     all_passed &= test_parsy_parse("  0001   002   003  ", "0001 002 003");
-//     all_passed &= test_parsy_parse("UPPER  and   lower", "UPPER and lower");
-//     all_passed &= test_parsy_parse("MiXeD   CaSe   Words", "MiXeD CaSe Words");
-//     all_passed &= test_parsy_parse("NoSpacesHere", "NoSpacesHere");
-//     all_passed &= test_parsy_parse("こんにちは  世界", "こんにちは 世界");
-//     all_passed &= test_parsy_parse("Emoji    😀😀  😀", "Emoji 😀😀 😀");
-//     all_passed &= test_parsy_parse("This                  has                  long                  spaces.", "This has long spaces.");
-//     all_passed &= test_parsy_parse("a b c d e", "a b c d e");
-//     all_passed &= test_parsy_parse("$ # & * ^", "$ # & * ^");
-//     all_passed &= test_parsy_parse("Th  isWor dHas   IntraWord    Spaces", "Th isWor dHas IntraWord Spaces");
-//     all_passed &= test_parsy_parse("@@@Hello   World", "@@@Hello World");
-//     all_passed &= test_parsy_parse("  Mix  of   spaces,  words,123,numbers   & special!   ", "Mix of spaces, words,123,numbers & special!");
-//     all_passed &= test_parsy_parse("a a a a a", "a a a a a");
-//     all_passed &= test_parsy_parse("aaaaaaaaaaaaa", "aaaaaaaaaaaaa");
-//     all_passed &= test_parsy_parse("Word\tTab  Spaces\nNewLine    Mixed", "Word Tab Spaces NewLine Mixed");
-//     all_passed &= test_parsy_parse("This          is          a          test", "This is a test");
-//     all_passed &= test_parsy_parse(",.-_=+|><:{}[]()", ",.-_=+|><:{}[]()");
-//     all_passed &= test_parsy_parse("                      ", "");
-//     all_passed &= test_parsy_parse("This is a very very very very very very very very very very very long string with lots and lots and lots and lots of words and spaces.", "This is a very very very very very very very very very very very long string with lots and lots and lots and lots of words and spaces.");
-//     all_passed &= test_parsy_parse("123abc 456def 789ghi", "123abc 456def 789ghi");
-//     all_passed &= test_parsy_parse("Space\\ followed by actual space ", "Space\\ followed by actual space");
-
-//     if (all_passed) {
-//         printf("\nAll tests passed!\n");
-//     } else {
-//         printf("\nSome tests failed. Please check the output.\n");
-//     }
-
-//     return 0;
-// }
